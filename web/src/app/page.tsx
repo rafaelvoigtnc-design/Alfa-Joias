2fce13f483c6187bb8a3e4a77a5198f7989498fa
Alfa Joias <contato@alfajoias.com.br>
2025-12-03 15:21:58 -0300

    descrição da alteração

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -17,6 +17,8 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <HeroCarousel />
+      {/* Faixa vermelha abaixo do banner */}
+      <div className="w-full bg-red-600 h-2"></div>
       <Categories />
       <FeaturedProducts />
       <Promotions />
